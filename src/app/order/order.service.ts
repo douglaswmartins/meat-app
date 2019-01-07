b2b8ccdeaeb97f6175137bde1fa38d3f47faf873
douglaswmartins <martins.douglas1997@outlook.com>
2019-01-07 16:52:28 -0200

    Finalização da compra, incluindo o metodo post

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
+
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
 import { ShoppingCartService } from './../restaurant-details/shopping-cart/shopping-cart.service';
+
 import { CartItem } from './../restaurant-details/shopping-cart/cart-item.model';
+import { Order, OrderItem } from './order.model';
+
+import { MEAT_API } from './../app.api';
 
 @Injectable()
 export class OrderService {
 
-  constructor(private cartService: ShoppingCartService) { }
+  constructor(private cartService: ShoppingCartService, private http: Http) { }
 
   cartItems(): CartItem[] {
     return this.cartService.items
@@ -25,5 +34,19 @@ export class OrderService {
 
   remove(item: CartItem) {
     this.cartService.removeItem(item)
+  }
+
+  clear() {
+    this.cartService.clear()
+  }
+
+  checkOrder(order: Order): Observable<string> {
+    const headers = new Headers()
+    headers.append('Content-Type', 'application/json')
+    return this.http.post(`${MEAT_API}/orders`, 
+                          JSON.stringify(order),
+                          new RequestOptions({headers: headers}))
+                    .map(response => response.json())
+                    .map(order => order.id)
   }
 }
