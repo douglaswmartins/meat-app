2a24ffc2112174b9b3646ac965799b19307275c7
douglaswmartins <martins.douglas1997@outlook.com>
2019-02-07 15:26:16 -0200

    errorHandler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgModule, LOCALE_ID, ErrorHandler } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, PreloadAllModules } from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {LocationStrategy, HashLocationStrategy} from '@angular/common'
 
+import { AplicationErrorHandler } from './app.error-handler';
 import {ROUTES} from './app.routes'
 
 import { AppComponent } from './app.component';
@@ -50,7 +51,8 @@ import { UserDetailComponent } from './header/user-detail/user-detail.component'
     SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
-  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'},
+              {provide: ErrorHandler, useClass: AplicationErrorHandler}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
